4f289129590466c8147a53ca19f7a68196624adc
ckonstanski <ckonstanski@pippiandcarlos.com>
2020-12-23 10:43:53 -0700

    initial import

---
+++ org-ckons-http.asd
@@ -0,0 +1,36 @@
+;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10 -*-
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
+(in-package #:cl)
+
+(defpackage #:org-ckons-http-system (:use #:cl #:asdf))
+(in-package #:org-ckons-http-system)
+
+(defmacro do-defsystem (&key name version maintainer author description long-description depends-on components)
+  `(defsystem ,name
+       :name ,name
+       :version ,version
+       :maintainer ,maintainer
+       :author ,author
+       :description ,description
+       :long-description ,long-description
+       :depends-on ,(eval depends-on)
+       :components ,components))
+
+(defparameter *quicklisp-packages* '(drakma))
+(defparameter *asdf-packages* '(org-ckons-core))
+(defparameter *all-packages* (append *quicklisp-packages* *asdf-packages*))
+
+(loop for pkg in *quicklisp-packages* do
+     (ql:quickload (symbol-name pkg)))
+
+(do-defsystem :name "org-ckons-http"
+              :version "1"
+              :maintainer "Carlos Konstanski <me@ckons.org>"
+              :author "Carlos Konstanski <me@ckons.org>"
+              :description "org-ckons-http"
+              :long-description "org-ckons-http is a library which provides HTML rendering functionality and a drakma HTTP client wrapper."
+              :depends-on *all-packages*
+              :components ((:module src
+                            :components ((:file "html")
+                                         (:file "http" :depends-on ("html"))))))
---
+++ src/http.lisp
@@ -0,0 +1,66 @@
+;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10 -*-
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
+(in-package #:org-ckons-http)
+
+(defmacro with-cookie-jar (&rest body)
+  `(let ((cookie-jar (make-instance 'drakma:cookie-jar)))
+     ,@body))
+
+(defun drakma-request (url
+                       cookie-jar
+                       &key
+                         (method :get)
+                         (content-type "application/x-www-form-urlencoded")
+                         (content nil)
+                         (user-agent :firefox)
+                         (redirect t)
+                         (auto-referer t)
+                         (additional-headers nil)
+                         (connection-timeout 20)
+                         (proxy nil)
+                         (proxy-basic-authorization nil)
+                         (basic-authorization nil))
+  (drakma:http-request url
+                       :cookie-jar cookie-jar
+                       :method method
+                       :content-type content-type
+                       :content content
+                       :user-agent user-agent
+                       :redirect redirect
+                       :auto-referer auto-referer
+                       :connection-timeout connection-timeout
+                       :additional-headers additional-headers
+                       :proxy proxy
+                       :proxy-basic-authorization proxy-basic-authorization
+                       :basic-authorization basic-authorization
+                       :close t))
+
+(defun escape-url (url)
+  (let ((escaped-url url))
+    (setf escaped-url (ppcre:regex-replace-all "%" escaped-url "%25"))
+    (setf escaped-url (ppcre:regex-replace-all "\\?" escaped-url "%3F"))
+    (setf escaped-url (ppcre:regex-replace-all "#" escaped-url "%23"))
+    (setf escaped-url (ppcre:regex-replace-all "/" escaped-url "%2F"))
+    (setf escaped-url (ppcre:regex-replace-all "'" escaped-url "%27"))
+    (setf escaped-url (ppcre:regex-replace-all " " escaped-url "%20"))
+    (setf escaped-url (ppcre:regex-replace-all "\\(" escaped-url "%28"))
+    (setf escaped-url (ppcre:regex-replace-all "\\)" escaped-url "%29"))
+    (setf escaped-url (ppcre:regex-replace-all ":" escaped-url "%3A"))
+    escaped-url))
+
+(defun unescape-url (url)
+  (let ((unescaped-url url))
+    (setf unescaped-url (ppcre:regex-replace-all "%25" unescaped-url "%"))
+    (setf unescaped-url (ppcre:regex-replace-all "%3F" unescaped-url "?"))
+    (setf unescaped-url (ppcre:regex-replace-all "%23" unescaped-url "#"))
+    (setf unescaped-url (ppcre:regex-replace-all "%2F" unescaped-url "/"))
+    (setf unescaped-url (ppcre:regex-replace-all "%27" unescaped-url "'"))
+    (setf unescaped-url (ppcre:regex-replace-all "%20" unescaped-url " "))
+    (setf unescaped-url (ppcre:regex-replace-all "%28" unescaped-url "("))
+    (setf unescaped-url (ppcre:regex-replace-all "%29" unescaped-url ")"))
+    (setf unescaped-url (ppcre:regex-replace-all "%3A" unescaped-url ":"))
+    unescaped-url))
+
+(defun newlines-to-backslash-n (body)
+  (cl-ppcre:regex-replace-all #\Newline body "\\\\n"))
